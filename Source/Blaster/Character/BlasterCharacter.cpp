ac65f8ffe044914a8ba7ba67d851421d0c93d0d4
cpclub1 <cpclub1@gmail.com>
2022-07-04 18:00:03 +0900

    WIP  ch 38

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -26,15 +26,39 @@ void ABlasterCharacter::BeginPlay()
 	
 }
 
+void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ABlasterCharacter::MoveForward(float Value)
+{
+	if( Controller != nullptr && Value!= 0.f)
+	{
+		const FRotator YawRotation( 0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction( FRotationMatrix( YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction,Value);
+		
+	}
+}
+
+void ABlasterCharacter::MoveRight(float Value)
+{
+}
+
+void ABlasterCharacter::Turn(float Value)
+{
+}
+
+void ABlasterCharacter::LookUp(float Value)
+{
+}
+
 void ABlasterCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
